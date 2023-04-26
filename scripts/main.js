d19cf835688ea6c0e79b046952978bc23f3dc84c
pacool1234 <niederleytnerenciso@gmail.com>
2023-04-26 17:20:32 +0200

    create main feed

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,8 @@ const API_URL = "http://localhost:8080/"
 let user = { id: "6445388da8130f4b9f500867" }
 
 ///////////////////////////////Variables Paco
-
-
-
-
+const mainFeed =  document.getElementById('main-feed');
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDQ4Zjg5NTY0ZTMzNDVkNGM4NGEzMTQiLCJpYXQiOjE2ODI1MDY5NjJ9.wyXaMIsOWZapkwcvZsM9FJooyZ6uRD4gX-JjRy4sboI';
 
 
 ///////////////////////////////////////////////////////////////KAT functions
@@ -16,7 +14,7 @@ let user = { id: "6445388da8130f4b9f500867" }
 
 //Axios get user info
 
-showUser();
+// showUser();
 
 async function showUser(){
   try{
@@ -29,4 +27,63 @@ async function showUser(){
   }
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////PACO functions
+///////////////////////////////////////////////////////////////////PACO functions
+
+// Display main feed
+async function displayMainFeed() {
+  try {
+    let friendsPosts = await axios.get(API_URL + 'posts/getFriendsPosts',{
+      headers: {
+        Authorization: token
+      }
+    });
+
+    friendsPosts = friendsPosts.data.posts;
+
+    friendsPosts.forEach(post => {
+      const col = document.createElement('div')
+      const card = document.createElement('div')
+      const img = document.createElement('img')
+      const cardBody = document.createElement('div')
+      const cardTitle = document.createElement('h5')
+      const cardMessage = document.createElement('p')
+      const form = document.createElement('form')
+      const input = document.createElement('input')
+      const button = document.createElement('button')
+
+      // Set classes and attributes
+      col.className = 'col'
+      card.className = 'card'
+      img.className = 'card-img-top'
+      cardBody.className = 'card-body'
+      cardTitle.className = 'card-title'
+      input.className = 'form-control'
+      button.className = 'btn btn-primary'
+
+      // img.src = post.image
+      img.setAttribute('src', 'http://localhost:8080/uploads/users/' + user.image)
+      cardTitle.textContent = post.title
+      cardMessage.textContent = post.body
+      input.placeholder = 'Write a comment...'
+      button.href = '#'
+      button.textContent = 'Comment'
+
+      // Append elements
+      cardBody.appendChild(cardTitle)
+      cardBody.appendChild(cardMessage)
+      cardBody.appendChild(input)
+      cardBody.appendChild(button)
+      card.appendChild(img)
+      card.appendChild(cardBody)
+      col.appendChild(card)
+      mainFeed.appendChild(col)
+    })
+
+
+    console.log(friendsPosts);        
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+displayMainFeed();
